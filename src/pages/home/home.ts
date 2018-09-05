2b32fec2a7cbed7ab9afb7feec4fbfd45c90ca1b
Nik Bornemeier <NikolausRB@gmail.com>
2018-09-05 14:57:46 -0600

    create refreshCoins method

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DataProvider } from '../../providers/data/data';
 import { Storage } from '@ionic/storage';
+import { Subscriber } from 'rxjs/Subscriber';
 
 @Component({
   selector: 'page-home',
@@ -9,12 +10,43 @@ import { Storage } from '@ionic/storage';
 })
 export class HomePage {
 
+  coins: Object;
+  likedCoins = [];
+
   constructor(public navCtrl: NavController, private _data: DataProvider, private storage: Storage) {
 
   }
 
   ionViewDidLoad() {
 
+  }
+
+  refreshCoins() {
+    this.storage.get('likedCoins').then((val) => {
+
+      //if the value is not set, then:
+      if (!val) {
+        this.likedCoins.push('BTC', 'ETH', 'IOT');
+        this.storage.set('likedCoins', this.likedCoins);
+
+        this._data.getCoins(this.likedCoins)
+          .subscribe(result => {
+            this.coins = result;
+          })
+      }
+
+      //It is set
+      else {
+        this.likedCoins = val;
+
+        this._data.getCoins(this.likedCoins)
+          .subscribe(result => {
+            this.coins = result;
+          })
+
+      }
+
+    })
   }
 
 }
