158fafc9bfde72e68026064f33fcb29377aba806
Nik Bornemeier <NikolausRB@gmail.com>
2018-09-06 15:00:09 -0600

    style remove button and slide funtionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DataProvider } from '../../providers/data/data';
 import { Storage } from '@ionic/storage';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'page-home',
@@ -9,9 +10,12 @@ import { Storage } from '@ionic/storage';
 })
 export class HomePage {
 
+  detailToggle = [];
   objectKeys = Object.keys;
   coins: Object;
+  details: Object;
   likedCoins = [];
+  chart = [];
 
   constructor(public navCtrl: NavController, private _data: DataProvider, private storage: Storage) {
     this.storage.remove('likedCoins');
@@ -30,8 +34,8 @@ export class HomePage {
     this.storage.get('likedCoins').then((val) => {
 
       // If the value is not set, then:
-      if(!val) {
-        this.likedCoins.push('BTC','ETH','IOT');
+      if (!val) {
+        this.likedCoins.push('BTC', 'ETH', 'IOT');
         this.storage.set('likedCoins', this.likedCoins);
 
         this._data.getCoins(this.likedCoins)
@@ -44,13 +48,83 @@ export class HomePage {
         this.likedCoins = val;
 
         this._data.getCoins(this.likedCoins)
-        .subscribe(res => {
-          this.coins = res;
-        })
+          .subscribe(res => {
+            this.coins = res;
+          })
       }
 
     })
 
   }
+
+  coinDetails(coin, index) {
+
+    if (this.detailToggle[index])
+      this.detailToggle[index] = false;
+    else {
+      this.detailToggle.fill(false);
+      this._data.getCoin(coin)
+        .subscribe(res => {
+          this.details = res['DISPLAY'][coin]['USD'];
+
+          this.detailToggle[index] = true;
+
+          this._data.getChart(coin)
+            .subscribe(res => {
+
+              console.log(res);
+
+              let coinHistory = res['Data'].map((a) => (a.close));
+
+              setTimeout(() => {
+                this.chart[index] = new Chart('canvas' + index, {
+                  type: 'line',
+                  data: {
+                    labels: coinHistory,
+                    datasets: [{
+                      data: coinHistory,
+                      borderColor: "#3cba9f",
+                      fill: false
+                    }
+                    ]
+                  },
+                  options: {
+                    tooltips: {
+                      callbacks: {
+                        label: function (tooltipItems, data) {
+                          return "$" + tooltipItems.yLabel.toString();
+                        }
+                      }
+                    },
+                    responsive: true,
+                    legend: {
+                      display: false
+                    },
+                    scales: {
+                      xAxes: [{
+                        display: false
+                      }],
+                      yAxes: [{
+                        display: false
+                      }],
+                    }
+                  }
+                });
+              }, 250);
+
+            });
+
+
+        });
+
+
+    }
+
+  }
+
+  swiped(index) {
+    this.detailToggle[index] = false
+  }
+
 
 }
